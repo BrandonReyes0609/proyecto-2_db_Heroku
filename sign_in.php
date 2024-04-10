c64bdac9076e2efab6631dfe1e3eca0fee875093
Carlos Valladares <val221164@uvg.edu.gt>
2024-04-10 00:34:53 -0600

    Añadido de parámetro al sign_in

--- sign_in.php
+++ sign_in.php
@@ -1,23 +1,24 @@
 <?php
 include 'includes/conexion.php';
 
-// Comprobar que se está haciendo una petición POST y que los campos requeridos están presentes
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nombre_usuario']) && isset($_POST['password'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nombre_usuario']) && isset($_POST['password']) && isset($_POST['rol'])) {
     // Recoger los datos del formulario
     $nombre_usuario = trim($_POST['nombre_usuario']);
-    $password = password_hash(trim($_POST['password']), PASSWORD_DEFAULT); // Hashear la password
+    $password = password_hash(trim($_POST['password']), PASSWORD_DEFAULT); // Hashear la contraseña
+    $rol = trim($_POST['rol']); // Recoger el rol del formulario
 
-    $stmt = $conn->prepare("INSERT INTO users (nombre_usuario, password) VALUES (:nombre_usuario, :password)");
+    // Preparar la consulta para insertar el nuevo usuario
+    $stmt = $conn->prepare("INSERT INTO users (username, contraseña, rol) VALUES (:nombre_usuario, :password, :rol)");
     $stmt->bindParam(':nombre_usuario', $nombre_usuario, PDO::PARAM_STR);
     $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+    $stmt->bindParam(':rol', $rol, PDO::PARAM_STR); // Asegurar de agregar el parámetro del rol
 
     try {
         $stmt->execute();
         echo "Usuario registrado exitosamente.";
-        header("Location: index.php");
+        header("Location: home.php");
         exit;
     } catch (PDOException $e) {
-        // Código de error de PostgreSQL para violaciones de la unicidad
         if ($e->getCode() == 23505) {
             echo "El nombre de usuario ya existe.";
         } else {
