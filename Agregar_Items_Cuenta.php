19d546d38446a5f01950cc023e57011be4f6e9ae
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-12 20:25:16 -0600

    1.1

--- Agregar_Items_Cuenta.php
+++ Agregar_Items_Cuenta.php
@@ -26,38 +26,28 @@ if (isset($_SESSION['user_alert'])) {
 </head>
 <body>
 
-<nav class="navbar navbar-expand-sm navbar-light bg-light">
-  <a class="navbar-brand" href="#">Mi Sitio</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="home.php">Inicio</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Crear_cuenta.php">Crear Cuenta</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Agregar_Items_Cuenta.php">Items Cuenta</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Pantalla_cocina.php">Pantalla Cocina</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#more">Más</a>
-      </li>
-    </ul>
-  </div>
-</div>
-</nav>
-
 <div class="home-container">
-    <h1>Agregar Item Cuenta</h1>
+    <h1>Bienvenido al Sistema</h1>
     <p>Estás autenticado como <?php echo htmlspecialchars($_SESSION['nombre_usuario']); ?></p>
     <p><a href="logout.php">Cerrar Sesión</a></p>
+
+    <form action="#.php">
+      <label for="lang">Ingrese la zona:</label>
+      <select name="tipo_zona" id="tipo_zona">
+        <option value="zona1">zona 1</option>
+        <option value="zona2">zona 2</option>
+        <option value="zona3">zona 3</option>
+        
+      <input type="checkbox" id="unir_mesa" name="unir_mesa" value="">
+      <span>Unir mesas</span>
+      <label for="lang">Ingrese No. mesa:</label>
+      <input type="text" id="numero_mesa" name="numero_mesa" value="">
+      <label for="lang">Ingrese la cantidad de personas:</label>
+      <input type="text" id="num_personas" name="num_personas" value="">
+      <input type="submit" value="Abrir Cuenta">
+
+      <br>
+    </form>
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
