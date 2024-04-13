a188dfcd35170b6cd281a6ccee2f5678367234db
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-12 20:29:38 -0600

    1.3

--- Agregar_Items_Cuenta.php
+++ Agregar_Items_Cuenta.php
@@ -1,10 +1,10 @@
 <?php
 session_start(); // Iniciar o continuar la sesión
 
-// Redirige al usuario a 'index.php' si no está autenticado
+// Verificar si el usuario está autenticado
 if (!isset($_SESSION['nombre_usuario'])) {
     header("Location: index.php");
-    exit; // Detiene la ejecución del script después de la redirección
+    exit;
 }
 
 // Almacenar mensaje de alerta en una variable y limpiar la sesión
@@ -20,14 +20,41 @@ if (isset($_SESSION['user_alert'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agregar Item Cuenta</title>
+    <title>Agregar Items</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/estilos.css">
 </head>
 <body>
 
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+  <a class="navbar-brand" href="#">Mi Sitio</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Crear_cuenta.php">Crear Cuenta</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Agregar_Items_Cuenta.php">Items Cuenta</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Pantalla_cocina.php">Pantalla Cocina</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#more">Más</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <div class="home-container">
-    <h1>Bienvenido al Sistema</h1>
+    <h1>Agregar Items</h1>
     <p>Estás autenticado como <?php echo htmlspecialchars($_SESSION['nombre_usuario']); ?></p>
     <p><a href="logout.php">Cerrar Sesión</a></p>
 
@@ -48,6 +75,7 @@ if (isset($_SESSION['user_alert'])) {
 
       <br>
     </form>
+
 </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
