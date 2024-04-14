8f0f00967eda6028fd536f12701ed73846deaedd
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-14 01:02:25 -0600

    1

--- Agregar_Items_Cuenta.php
+++ Agregar_Items_Cuenta.php
@@ -65,12 +65,13 @@
       <form action="#.php">
       <label for="tipo_cuenta">Seleccione la cuenta:</label>
         <select name="tipo_cuenta" id="tipo_cuenta">
-        <?php
-          while($obj = pg_fetch_object($consulta)){?>
-            <option value="<?php echo $obj->cuenta_id; ?>"><?php echo $obj->cuenta_id; ?></option>
-        <?php
-          }
-        ?>
+          <?php
+            while($obj = pg_fetch_object($consulta)){?>
+              <option value="<?php echo $obj->cuenta_id ?>"><?php $obj->cuenta_id;?></option>
+            <?php
+            }
+    
+          ?>
         </select>
       <form action="app/mesero/procesar_items.php" method="post">
           <label for="tipo_cuenta">Selecciones la cuenta:</label>
