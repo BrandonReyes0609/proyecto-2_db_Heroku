1475acffaafa1b03bf5c9f480dbb6bba0589e5af
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-14 02:34:26 -0600

    2

--- Envio_item_cuenta.php
+++ Envio_item_cuenta.php
@@ -2,7 +2,17 @@
 session_start();
 
 require 'includes/conexion.php'; // Verifica que la ruta es correcta
+$host = "cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+$database = "dceql5bo9j3plb";
+$user = "u1e25j4kkmlge1";
+$port = "5432";
+$password = "p4ac621d657dad701bc6ed9505ad96894fe1a390fd1e05ef41b37334c60753c5b";
 
+// Crear la cadena de conexión
+$dsn = "host=$host port=$port dbname=$database user=$user password=$password";
+
+// Establecer conexión
+$conn = pg_connect($dsn);
 $tipo_cuenta = $_REQUEST['tipo_cuenta'];
 $tipo_plato = $_REQUEST['tipo_plato'];
 $num_platos = $_REQUEST['num_platos'];
