0b2fb88987ad5fa32a4170eb9d4522a2333e7af2
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-14 02:30:50 -0600

    2

--- Envio_item_cuenta.php
+++ Envio_item_cuenta.php
@@ -11,28 +11,25 @@ $num_bebida = $_REQUEST['num_bebida'];
 
 // Prepara las consultas SQL para platos y bebidas
 $query_platos = "INSERT INTO items_cuenta (cuenta_id, item_id, cantidad, fecha_hora, cocinado) VALUES ($1, $2, $3, NOW(), false)";
-$query_bebidas = "INSERT INTO items_cuenta (cuenta_id, item_id, cantidad, fecha_hora, cocinado) VALUES ($1, $2, $3, NOW(), true)";
-
-// Ejecuta la primera consulta
 $resultado1 = pg_query_params($conn, $query_platos, array($tipo_cuenta, $tipo_plato, $num_platos));
-if (!$resultado1) {
-    $_SESSION['user_alert'] = "Error en la inserción de platos: " . pg_last_error($conn);
-    header('Location: error.php');
-    exit();
-}
 
-// Ejecuta la segunda consulta
-$resultado2 = pg_query_params($conn, $query_bebidas, array($tipo_cuenta, $tipo_bbeida, $num_bebida));
-if (!$resultado2) {
-    $_SESSION['user_alert'] = "Error en la inserción de bebidas: " . pg_last_error($conn);
-    header('Location: error.php');
-    exit();
-}
 
+if ($resultado1) {
+    $query_bebidas = "INSERT INTO items_cuenta (cuenta_id, item_id, cantidad, fecha_hora, cocinado) VALUES ($1, $2, $3, NOW(), true)";
+    $resultado2 = pg_query_params($conn, $query_cuentas, array($tipo_cuenta, $tipo_bbeida, $num_bebida));
+
+    if ($resultado2) {
+        $_SESSION['user_alert'] = "Se enviaron los datos correctamente";
+    } else {
+        $_SESSION['user_alert'] = "Error resultado 2: " . pg_last_error($conn);
+    }
+} else {
+    $_SESSION['user_alert'] = "Error resultado 1: " . pg_last_error($conn);
+}
 // Cierra la conexión
 pg_close($conn);
 
 // Redirecciona si todo fue exitoso
-header('Location: Envio_item_cuenta.php');
+header('Location: Agregar_Items_Cuenta.php');
 exit();
 ?>
