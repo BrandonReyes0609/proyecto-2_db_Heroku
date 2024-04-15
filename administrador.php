fae6358b61b793582792ad2dd32b1be501e10db5
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2024-04-15 14:25:27 -0600

    Hoy creo que si funciona..

--- administrador.php
+++ administrador.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION['nombre_usuario'])) {
     <meta charset="UTF-8">
     <title>Reportes del Restaurante</title>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Estilos aquí -->
     <style>
         body { padding: 20px; }
         .form-group { margin-bottom: 15px; }
@@ -36,8 +37,8 @@ if (!isset($_SESSION['nombre_usuario'])) {
     <div class="container">
         <h1 class="mb-4">Reportes del Restaurante</h1>
         <form method="post" class="mb-4">
-            <div class="form-row align-items-end"> <!-- Bootstrap class to align items vertically -->
-                <div class="col-md-3 mb-3"> <!-- Bootstrap responsive grid -->
+            <div class="form-row align-items-end">
+                <div class="col-md-3 mb-3">
                     <label for="fecha_inicio">Fecha de inicio:</label>
                     <input type="date" id="fecha_inicio" name="fecha_inicio" class="form-control" required>
                 </div>
@@ -56,7 +57,6 @@ if (!isset($_SESSION['nombre_usuario'])) {
 
         <?php
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            require 'includes/conexion.php';
             $fecha_inicio = $_POST['fecha_inicio'];
             $fecha_fin = $_POST['fecha_fin'];
             $reporte = $_POST['reporte'];
@@ -64,26 +64,6 @@ if (!isset($_SESSION['nombre_usuario'])) {
             switch ($reporte) {
                 case "platos_mas_pedidos":
                     platosMasPedidos($fecha_inicio, $fecha_fin, $conn);
-            $sql = "SELECT item_id as id, nombre, count(*) as pedidos_totales
-                    FROM cuentas c
-                    JOIN items_cuenta ic ON ic.cuenta_id = c.cuenta_id
-                    JOIN platos ON platos.plato_id = ic.item_id
-                    WHERE c.fecha_cierre BETWEEN :fecha_inicio AND :fecha_fin
-                    GROUP BY id, nombre
-                    ORDER BY pedidos_totales DESC";
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam("ss", $fecha_inicio, $fecha_fin);
-            $stmt->execute();
-            $result = $stmt->get_result();
-
-            echo "<h2 class='mt-5'>Platos Más Pedidos</h2>";
-            echo "<table class='table table-striped'>";
-            echo "<thead class='header'><tr><th>ID</th><th>Nombre</th><th>Pedidos Totales</th></tr></thead>";
-            echo "<tbody>";
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr><td>{$row['id']}</td><td>{$row['nombre']}</td><td>{$row['pedidos_totales']}</td></tr>";
-            }
-            echo "</tbody></table>";
                     break;
                 case "horario_mas_pedidos":
                     horarioMasPedidos($fecha_inicio, $fecha_fin, $conn);
