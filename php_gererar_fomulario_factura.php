857167cc89dc3c1903786ace9b16753676fae209
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-15 16:26:17 -0600

    2

--- php_gererar_fomulario_factura.php
+++ php_gererar_fomulario_factura.php
@@ -1,6 +1,44 @@
 <?php
 require('fpdf.php');
 
+require 'includes/conexion.php'; // Incluir el script de conexión desde la carpeta includes
+$host = "cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+$database = "dceql5bo9j3plb";
+$user = "u1e25j4kkmlge1";
+$port = "5432";
+$password = "p4ac621d657dad701bc6ed9505ad96894fe1a390fd1e05ef41b37334c60753c5b";
+
+// Crear la cadena de conexión
+$dsn = "host=$host port=$port dbname=$database user=$user password=$password";
+
+// Establecer conexión
+$conn = pg_connect($dsn);
+
+if (isset($_REQUEST['tipo_cuenta']) && is_numeric($_REQUEST['tipo_cuenta'])) {
+    $tipo_cuenta = $_REQUEST['tipo_cuenta'];
+    echo("exito");
+
+    $query_cuentas = "SELECT cuentas.cuenta_id,cuentas.mesa_id,cuentas.fecha_apertura,cuentas.fecha_cierre,cuentas.total,items_cuenta.item_id,items_cuenta.cantidad,items_cuenta.fecha_hora,items_cuenta.cocinado,platos.plato_id,platos.nombre,platos.descripcion,platos.precio,platos.tipo,(items_cuenta.cantidad * platos.precio) AS total_item FROM cuentas INNER JOIN items_cuenta ON cuentas.cuenta_id = items_cuenta.cuenta_id INNER JOIN platos ON items_cuenta.item_id = platos.plato_id WHERE cuentas.cuenta_id = $tipo_cuenta";
+    $query_total_cuenta = "SELECT cuentas.cuenta_id, SUM(items_cuenta.cantidad * platos.precio) AS sumatoria_total_items FROM cuentas INNER JOIN items_cuenta ON cuentas.cuenta_id = items_cuenta.cuenta_id INNER JOIN platos ON items_cuenta.item_id = platos.plato_id WHERE cuentas.cuenta_id = $tipo_cuenta GROUP BY cuentas.cuenta_id";
+    $query_cerrar_cuenta = "UPDATE cuentas SET fecha_cierre = NOW() WHERE cuenta_id = $1";
+    //$resultado1 = pg_query_params($conn, $query_cuentas, array($tipo_cuenta));
+    $consulta_pedidos1 = pg_query($conn,$query_cuentas);
+    $consulta_pedidos2 = pg_query($conn,$query_total_cuenta);
+
+    $resultado_query_cerrar_cuenta  = pg_query_params($conn, $query_cerrar_cuenta, array($tipo_cuenta));
+    if ($resultado_query_cerrar_cuenta) {
+        $_SESSION['user_alert'] = "Se enviaron los datos correctamente";
+    } else {
+        $_SESSION['user_alert'] = "Error resultado 2: " . pg_last_error($conn);
+    }
+    
+} else {
+    // Manejo de error si 'tipo_cuenta' no está presente o no es válido
+    echo "Tipo de cuenta no especificado o inválido.";
+    $tipo_cuenta = null; // Asegurarse de que no se proceda con un valor inválido
+}
+
+
 // Crear una nueva instancia de PDF
 $pdf = new FPDF();
 $pdf->AddPage();
