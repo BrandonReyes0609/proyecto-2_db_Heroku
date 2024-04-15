87fbc6922f1c9f0f9f43ccb1d2f4965cfa2a6b1b
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-14 19:48:24 -0600

    haber2

--- Consulta_items_cuenta.php
+++ Consulta_items_cuenta.php
@@ -21,8 +21,8 @@
         //echo htmlspecialchars($tipo_cuenta);
 
         // Prepara las consultas SQL para platos y bebidas
-        $query_cuentas = "SELECT * FROM items_cuenta WHERE cuenta_id = $tipo_cuenta";
-        //$query_platos = "SELECT cuentas.cuenta_id,cuentas.mesa_id,cuentas.fecha_apertura,cuentas.fecha_cierre,cuentas.total,items_cuenta.item_id,items_cuenta.cantidad,items_cuenta.fecha_hora,items_cuenta.cocinado,platos.plato_id,platos.nombre,platos.descripcion,platos.precio,platos.tipo,(items_cuenta.cantidad * platos.precio) AS total_item FROM cuentas INNER JOIN items_cuenta ON cuentas.cuenta_id = items_cuenta.cuenta_id INNER JOIN platos ON items_cuenta.item_id = platos.plato_id WHERE cuentas.cuenta_id = $1";
+        ///$query_cuentas = "SELECT * FROM items_cuenta WHERE cuenta_id = $tipo_cuenta";
+        $query_platos = "SELECT cuentas.cuenta_id,cuentas.mesa_id,cuentas.fecha_apertura,cuentas.fecha_cierre,cuentas.total,items_cuenta.item_id,items_cuenta.cantidad,items_cuenta.fecha_hora,items_cuenta.cocinado,platos.plato_id,platos.nombre,platos.descripcion,platos.precio,platos.tipo,(items_cuenta.cantidad * platos.precio) AS total_item FROM cuentas INNER JOIN items_cuenta ON cuentas.cuenta_id = items_cuenta.cuenta_id INNER JOIN platos ON items_cuenta.item_id = platos.plato_id WHERE cuentas.cuenta_id = $tipo_cuenta";
         //$resultado1 = pg_query_params($conn, $query_cuentas, array($tipo_cuenta));
         $consulta_pedidos1 = pg_query($conn,$query_cuentas);
     
@@ -61,27 +61,43 @@
   <div class="home-container">
       <h1>Impresion Pedidos</h1>
 
-
       <table>
         <thead>
           <tr>
-            <th>cuenta_id</th>
+            <th>cuenta_idmesa_id</th>
+            <th>fecha_apertura</th>
+            <th>fecha_cierre</th>
+            <th>total</th>
             <th>item_id</th>
             <th>cantidad</th>
             <th>fecha_hora</th>
             <th>cocinado</th>
-            
+            <th>plato_id</th>
+            <th>nombre</th>
+            <th>descripcion</th>
+            <th>precio</th>
+            <th>tipo</th>
+            <th>total_item</th>
           </tr>
         </thead>
         <tbody>
         <?php
             while($obj = pg_fetch_object($consulta_pedidos1)){ ?>
               <tr>
-                <td><?php echo $obj->cuenta_id;?></td>
-                <td><?php echo $obj->item_id;?></td>
-                <td><?php echo $obj->cantidad;?></td>
-                <td><?php echo $obj->fecha_hora;?></td>
-                <td><?php echo $obj->cocinado ;?></td>
+                <td><?php echo($obj->cuenta_idmesa_id);?></td>
+                <td><?php echo($obj->fecha_apertura);?></td>
+                <td><?php echo($obj->fecha_cierre);?></td>
+                <td><?php echo($obj->total);?></td>
+                <td><?php echo($obj->item_id);?></td>
+                <td><?php echo($obj->cantidad);?></td>
+                <td><?php echo($obj->fecha_hora);?></td>
+                <td><?php echo($obj->cocinado);?></td>
+                <td><?php echo($obj->plato_id);?></td>
+                <td><?php echo($obj->nombre);?></td>
+                <td><?php echo($obj->descripcion);?></td>
+                <td><?php echo($obj->precio);?></td>
+                <td><?php echo($obj->tipo);?></td>
+                <td><?php echo($obj->total_item);?></td>
               </tr>
             </tbody>
           <?php
