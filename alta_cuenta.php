e161939cf5acbf49af7bf5fa65a61ad75576eaa7
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-14 21:43:39 -0600

    nova 1

--- alta_cuenta.php
+++ alta_cuenta.php
@@ -1,32 +1,39 @@
 <?php
-session_start();
+    session_start();
 
-require 'includes/conexion.php'; // Asegúrate de que la ruta esté correcta
+    require 'includes/conexion.php'; // Incluir el script de conexión desde la carpeta includes
+    $host = "cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+    $database = "dceql5bo9j3plb";
+    $user = "u1e25j4kkmlge1";
+    $port = "5432";
+    $password = "p4ac621d657dad701bc6ed9505ad96894fe1a390fd1e05ef41b37334c60753c5b";
 
-$tipo_area1 = $_REQUEST['tipo_zona'];
-$num_personas = $_REQUEST['num_personas'];
-$numero_mesa = $_REQUEST['numero_mesa'];
+    // Crear la cadena de conexión
+    $dsn = "host=$host port=$port dbname=$database user=$user password=$password";
+    $tipo_area1 = $_REQUEST['tipo_zona'];
+    $num_personas = $_REQUEST['num_personas'];
+    $numero_mesa = $_REQUEST['numero_mesa'];
 
-// Usamos la conexión de la base de datos que se creó en conexion.php
-$query_mesas = "INSERT INTO mesas (mesa_id, area_id, capacidad, movilidad) VALUES ($1, $2, $3, false)";
-$resultado1 = pg_query_params($conn, $query_mesas, array($numero_mesa, $tipo_area1, $num_personas));
+    // Usamos la conexión de la base de datos que se creó en conexion.php
+    $query_mesas = "INSERT INTO mesas (mesa_id, area_id, capacidad, movilidad) VALUES ($1, $2, $3, false)";
+    $resultado1 = pg_query_params($conn, $query_mesas, array($numero_mesa, $tipo_area1, $num_personas));
 
-if ($resultado1) {
-    $query_cuentas = "INSERT INTO cuentas (mesa_id, fecha_apertura, fecha_cierre, total) VALUES ($1, NOW(), null, 0)";
-    $resultado2 = pg_query_params($conn, $query_cuentas, array($numero_mesa));
+    if ($resultado1) {
+        $query_cuentas = "INSERT INTO cuentas (mesa_id, fecha_apertura, fecha_cierre, total) VALUES ($1, NOW(), null, 0)";
+        $resultado2 = pg_query_params($conn, $query_cuentas, array($numero_mesa));
 
-    if ($resultado2) {
-        $_SESSION['user_alert'] = "Se enviaron los datos correctamente";
+        if ($resultado2) {
+            $_SESSION['user_alert'] = "Se enviaron los datos correctamente";
+        } else {
+            $_SESSION['user_alert'] = "Error resultado 2: " . pg_last_error($conn);
+        }
     } else {
-        $_SESSION['user_alert'] = "Error resultado 2: " . pg_last_error($conn);
+        $_SESSION['user_alert'] = "Error resultado 1: " . pg_last_error($conn);
     }
-} else {
-    $_SESSION['user_alert'] = "Error resultado 1: " . pg_last_error($conn);
-}
 
-pg_close($conn);
+    pg_close($conn);
 
-// Almacenar mensaje y redirigir al usuario a la página 'Crear_cuenta.php'
-header('Location: Crear_cuenta.php');
-exit(); // No olvides llamar a exit() después de header()
+    // Almacenar mensaje y redirigir al usuario a la página 'Crear_cuenta.php'
+    header('Location: Crear_cuenta.php');
+    exit(); // No olvides llamar a exit() después de header()
 ?>
