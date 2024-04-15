7376c298daa95ba84498e6b6e7b7519efa38f32d
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-14 18:55:46 -0600

    4

--- Consulta_items_cuenta.php
+++ Consulta_items_cuenta.php
@@ -1,21 +1,17 @@
 <?php
-session_start();
+    $host = "cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
+    $database = "dceql5bo9j3plb";
+    $user = "u1e25j4kkmlge1";
+    $port = "5432";
+    $password = "p4ac621d657dad701bc6ed9505ad96894fe1a390fd1e05ef41b37334c60753c5b";
+    
+    // Crear la cadena de conexión
+    $dsn = "host=$host port=$port dbname=$database user=$user password=$password";
+    
+    // Establecer conexión
+    $conn = pg_connect($dsn);
 
-require 'includes/conexion.php'; // Verifica que la ruta es correcta
-$host = "cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com";
-$database = "dceql5bo9j3plb";
-$user = "u1e25j4kkmlge1";
-$port = "5432";
-$password = "p4ac621d657dad701bc6ed9505ad96894fe1a390fd1e05ef41b37334c60753c5b";
 
-// Crear la cadena de conexión
-$dsn = "host=$host port=$port dbname=$database user=$user password=$password";
-
-// Establecer conexión
-$conn = pg_connect($dsn);
-
-// Establecer conexión
-$conn = pg_connect($dsn);
 if (!$conn) {
     die("Error: No se pudo establecer conexión con la base de datos.");
 }
