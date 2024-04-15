070116b77ac5f602ad0458cfe513d33791fb6249
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2024-04-15 16:47:20 -0600

    test

--- php_gererar_fomulario_factura.php
+++ php_gererar_fomulario_factura.php
@@ -3,20 +3,19 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require 'includes/conexion.php';  
-require('fpdf.php');  
+require('tfpdf/fpdf.php');  // Cambio a tFPDF para soporte de UTF-8
 
-// Crear instancia de la clase FPDF
-$pdf = new FPDF('P', 'mm', 'A4');
+// Crear instancia de la clase tFPDF
+$pdf = new tFPDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 16);
+$pdf->AddFont('DejaVu','','DejaVuSansCondensed.ttf',true);  // Agregar fuente DejaVu que soporta UTF-8
+$pdf->SetFont('DejaVu', '', 14);  // Usar fuente DejaVu con tamaño 14
 
 // Título del Documento
 $pdf->Cell(190, 10, 'Factura de Pedido', 0, 1, 'C');  // Título centrado
 
 // Espacio adicional
 $pdf->Ln(10);
-
-$pdf->SetFont('Arial', '', 12);  // Cambiar la fuente para el contenido
 
 // Verificar la presencia y validez de 'tipo_cuenta'
 if (isset($_POST['tipo_cuenta']) && is_numeric($_POST['tipo_cuenta'])) {
@@ -30,13 +29,10 @@ if (isset($_POST['tipo_cuenta']) && is_numeric($_POST['tipo_cuenta'])) {
 
     // Encabezados de la tabla de pedidos
     $pdf->SetFillColor(232, 232, 232);  // Color gris para el fondo de la celda
-    $pdf->SetFont('Arial', 'B', 12);  // Fuente en negrita para los encabezados
     $pdf->Cell(46, 10, 'Plato', 1, 0, 'C', true);
     $pdf->Cell(30, 10, 'Cantidad', 1, 0, 'C', true);
     $pdf->Cell(40, 10, 'Precio Unit.', 1, 0, 'C', true);
     $pdf->Cell(40, 10, 'Subtotal', 1, 1, 'C', true);  // El '1' al final indica el salto de línea
-
-    $pdf->SetFont('Arial', '', 12);  // Fuente normal para los datos
 
     // Agregar datos al PDF
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -60,7 +56,6 @@ if (isset($_POST['tipo_cuenta']) && is_numeric($_POST['tipo_cuenta'])) {
 
 // Datos adicionales del cliente
 $pdf->Ln(10);  // Espacio adicional antes de los datos del cliente
-$pdf->SetFont('Arial', 'I', 12);  // Fuente cursiva para los datos del cliente
 $pdf->Cell(95, 10, "Nombre Cliente: " . $_POST['nombre_cliente'], 0, 0);
 $pdf->Cell(95, 10, "NIT Cliente: " . $_POST['nit_cliente'], 0, 1);
 $pdf->Cell(95, 10, "Direccion Cliente: " . $_POST['direccion_cliente'], 0, 0);
