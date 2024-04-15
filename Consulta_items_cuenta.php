abd63e53c6b6e1a2e776a2257adae597a52c04bb
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-14 19:44:35 -0600

    haber

--- Consulta_items_cuenta.php
+++ Consulta_items_cuenta.php
@@ -21,7 +21,7 @@
         //echo htmlspecialchars($tipo_cuenta);
 
         // Prepara las consultas SQL para platos y bebidas
-        $query_cuentas = "SELECT * FROM items_cuenta WHERE cuenta_id = 8";
+        $query_cuentas = "SELECT * FROM items_cuenta WHERE cuenta_id = $tipo_cuenta";
         //$query_platos = "SELECT cuentas.cuenta_id,cuentas.mesa_id,cuentas.fecha_apertura,cuentas.fecha_cierre,cuentas.total,items_cuenta.item_id,items_cuenta.cantidad,items_cuenta.fecha_hora,items_cuenta.cocinado,platos.plato_id,platos.nombre,platos.descripcion,platos.precio,platos.tipo,(items_cuenta.cantidad * platos.precio) AS total_item FROM cuentas INNER JOIN items_cuenta ON cuentas.cuenta_id = items_cuenta.cuenta_id INNER JOIN platos ON items_cuenta.item_id = platos.plato_id WHERE cuentas.cuenta_id = $1";
         //$resultado1 = pg_query_params($conn, $query_cuentas, array($tipo_cuenta));
         $consulta_pedidos1 = pg_query($conn,$query_cuentas);
