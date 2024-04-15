c008f2d58864eeb78caa054a9f33c18c4e802afe
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-15 13:07:12 -0600

    7

--- envio_formulario.php
+++ envio_formulario.php
@@ -36,15 +36,20 @@ echo($direccion_queja);
 echo($queja_comida);
 echo($nombre_encuestado);
 */
-$insert_encuesta_queja = "INSERT INTO quejas (cliente_nombre, fecha, motivo, puntuacion, plato_nombre, mesero_id) VALUES ($1, $2, $3, $4, 5, 1)";
-$resultado2 = pg_query_params($conn, $insert_encuesta_queja, array($nombre_encuestado, NOW(), $comentario, $calificacion_queja));
-
+$insert_encuesta_queja = "INSERT INTO quejas (cliente_nombre, fecha, motivo, puntuacion, plato_nombre, mesero_id) VALUES ($_REQUEST['nombre_encuestado'], NOW(),$_REQUEST['comentario'] , $_REQUEST['calificacion_queja'], $_REQUEST['queja_comida'], $_REQUEST['amabilidad_mesero'])";
+//$resultado2 = pg_query_params($conn, $insert_encuesta_queja, array($nombre_encuestado, NOW(), $comentario, $calificacion_queja));
+$resultado2 = pg_query($conn,$insert_encuesta_queja) 
+/*
 if ($resultado2) {
     $_SESSION['user_alert'] = "Se enviaron los datos correctamente";
 } else {
     $_SESSION['user_alert'] = "Error resultado 2: " . pg_last_error($conn);
+    
 }
+*/
+echo("se enviaron los datos");
 /*
+
 
 $insert_encuesta_meseros = "INSERT INTO encuesta_mesero (mesero_id, puntuacion_amabilidad, puntuacion_exactitud, fecha_encuesta)VALUES ($1, $2, $3, $4)";
 
