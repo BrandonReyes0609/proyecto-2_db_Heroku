2dc99dcfb5377a7401680a882fb3f9d865d9837a
Carlos Valladares <115051589+vgcarlol@users.noreply.github.com>
2024-04-15 00:34:42 -0600

    Update factura.php

--- factura.php
+++ factura.php
@@ -13,13 +13,6 @@ if (!isset($_SESSION['nombre_usuario'])) {
     exit;
 }
 
-$cuenta_id = '';
-if (isset($_GET['cuenta_id'])) {
-    $cuenta_id = $_GET['cuenta_id'];
-} else {
-    // Manejar el caso de cuenta_id no definido antes de generar el PDF
-    die('El ID de la cuenta no está definido.');
-}
 
 $pdf = new TCPDF();
 
