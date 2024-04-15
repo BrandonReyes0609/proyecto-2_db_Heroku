93961e98f88fa49e776707eaf2697aeddfa37f04
BrandonReyes0609 <rey22992@uvg.edu.gt>
2024-04-15 13:51:08 -0600

    3

--- envio_formulario.php
+++ envio_formulario.php
@@ -15,29 +15,45 @@ $dsn = "host=$host port=$port dbname=$database user=$user password=$password";
 // Establecer conexión
 $conn = pg_connect($dsn);
 
-$mesero = $_REQUEST['mesero'];
+$mesero = (int) ($_REQUEST['mesero']);
 $amabilidad_mesero = $_REQUEST['amabilidad_mesero'];
-$calificacion_pedido = $_REQUEST['calificacion_pedido'];
+$calificacion_pedido = (int) ($_REQUEST['calificacion_pedido']);
 $comentario = $_REQUEST['comentario'];
-$calificacion_queja = $_REQUEST['calificacion_queja'];
+$calificacion_queja = (int) ($_REQUEST['calificacion_queja']);
 $direccion_queja = $_REQUEST['direccion_queja'];
 $queja_comida = $_REQUEST['queja_comida'];
+
 $nombre_encuestado = $_REQUEST['nombre_encuestado'];
 
-$insert_encuesta_meseros = "INSERT INTO encuesta_mesero (mesero_id, puntuacion_amabilidad, puntuacion_exactitud, fecha_encuesta) VALUES ($1, $2, $3, NOW())";
 
-$resultado1 = pg_query_params($conn, $insert_encuesta_meseros, array($mesero, $amabilidad_mesero, $calificacion_pedido));
 
-if ($resultado1) {
-    $insert_encuesta_queja = "INSERT INTO quejas (cliente_nombre, fecha, motivo, puntuacion, plato_nombre, mesero_id) VALUES ($1, NOW(), $2, $3, $4, $5)";
-    $resultado2 = pg_query_params($conn, $insert_encuesta_queja, array($nombre_encuestado, $comentario, $calificacion_queja, $queja_comida, $mesero));
+echo($mesero);
+echo($amabilidad_mesero);
+echo($calificacion_pedido);
+echo($comentario);
+echo($calificacion_queja);
+echo($direccion_queja);
+echo($queja_comida);
+echo($nombre_encuestado);
+var_dump($mesero);
+var_dump($amabilidad_mesero);
+var_dump($calificacion_pedido);
+var_dump($comentario);
+var_dump($calificacion_queja);
+var_dump($direccion_queja);
+var_dump($queja_comida);
+var_dump($nombre_encuestado);
+$insert_encuesta_queja = "INSERT INTO quejas (cliente_nombre, fecha, motivo, puntuacion, plato_nombre, mesero_id) VALUES ($1, NOW(), $2, $3, $4, $5)";
+$params = array($nombre_encuestado, $comentario, $calificacion_queja, $queja_comida, $mesero);
 
-    if ($resultado2) {
-        $_SESSION['user_alert'] = "Se enviaron los datos correctamente";
-    } else {
-        $_SESSION['user_alert'] = "Error resultado 2: " .
-    
+$resultado2 = pg_query_params($conn, $insert_encuesta_queja, $params);
+
+if ($resultado2 === false) {
+    echo "Error en la consulta: " . pg_last_error($conn);
+} else {
+    echo "Registro insertado correctamente.";
 }
+
 
 pg_close();
 echo("se enviaron los datos");
